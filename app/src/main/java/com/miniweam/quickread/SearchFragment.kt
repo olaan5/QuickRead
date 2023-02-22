d055f39e68ac02064ce272561621ed9f3cc2bf15
Joseph <j.t.cartoonist@gmail.com>
2023-02-22 21:51:06 +0100

    setup the adapters for each screens and loaded some dummy data for demonstration also made am few UI changes to some parts

--- app/src/main/java/com/miniweam/quickread/SearchFragment.kt
+++ app/src/main/java/com/miniweam/quickread/SearchFragment.kt
@@ -5,32 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.miniweam.quickread.DummyItem.getCategories
-import com.miniweam.quickread.DummyItem.getData
 import com.miniweam.quickread.databinding.FragmentHomeBinding
+import com.miniweam.quickread.databinding.FragmentSearchBinding
 
-class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
-    private val categoryAdapter by lazy { FeedsCategoryAdapter() }
-    private val feedsAdapter by lazy { FeedsAdapter() }
+    private val feedsAdapter by lazy { FeedsSearchAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.newsRecyclerView.adapter = feedsAdapter
-        binding.categoryRecyclerView.adapter = categoryAdapter
+        feedsAdapter.submitList(DummyItem.getData())
 
-        feedsAdapter.submitList(getData())
-        categoryAdapter.submitList(getCategories())
+        // TODO: Add a adapter for recent search then add the logic to alternate between the two recycler views
+
 
     }
 }
